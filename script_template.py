bff6c76ba4a426c48cdb6436df20eab92b0f0c8f
Chris Zaleski <zaleski.chris@gmail.com>
2022-11-01 15:37:09 -0400

    Added docstrings

--- script_template.py
+++ script_template.py
@@ -1,18 +1,44 @@
 #!/usr/bin/env python3
-# Description: 
-# Author: Chris Zaleski
-# Date: 
+"""
+File name: foobar.py
+Description: Does some amazing things, then some more amazing
+    things, then some more. Wow it's amazing.
+Author: Chris Zaleski
+Python Version: 3.x
+Date: 2022-08-22
+
+This script requires that "some_module" be installed within the Python
+environment you are running this script in.
+"""
 
 import sys
 import argparse
 import logging
 import pathlib
-    
-def main():
+
+def new_function(parameter_a: int = 1) -> str:
+    """
+    Do <this> and return <that>.
+
+    Parameters
+    ----------
+    parameter_a : int
+        The number of times to print a message.
+
+    Returns
+    -------
+    str
+        A string saying something cool.
+    """
+    for x in range(parameter_a):
+        logging.error('Test Error message ' + str(x+1))
+    return 'Did it!'
+
+def main() -> 1:
     # Parse command line arguments
     parser = argparse.ArgumentParser(description='Please use command line options in the form --commands')
     log_levels = ('DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL')
-    parser.add_argument('--log_level', default='WARNING', choices=log_levels, 
+    parser.add_argument('--log_level', default='DEBUG', choices=log_levels, 
         help='Verbosity of logging: DEBUG, INFO, WARNING, ERROR, CRITICAL')
     parser.add_argument('--log_file', type=pathlib.Path, default=None,
         help='Log file location. If not specified, log messages will be printed to the screen.')
@@ -25,11 +51,10 @@ def main():
         logging.basicConfig(format='%(levelname)s:%(message)s',
             level=args.log_level, stream=sys.stdout)
     
+    
     # Functionality goes here
     logging.info('Started script - %s', 'newScript.py')
-    logging.error('Test Error message')
-
-
+    print(new_function(3))
     return 0
 
 
