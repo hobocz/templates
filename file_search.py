02ae216b5630dd148972da0bf395ad411eca7a7a
Chris Zaleski <zaleski.chris@gmail.com>
2022-11-02 16:24:50 -0400

    Initial commit

--- file_search.py
+++ file_search.py
@@ -1,22 +1,22 @@
 #!/usr/bin/env python3
 """
-File name: foobar.py
-Description: Does some amazing things, then some more amazing
-    things, then some more. Wow it's amazing.
+File name: file_search.py
+Description: Performs a simple file search given a starting directory and 
+    a search string. Can optionally return only the newest file (latest timestamp)
+    in a directory if multiple files are found within that directory.
 Author: Chris Zaleski
 Python Version: 3.x
-Date: 2022-08-22
+Date: 2022-11-22
 
-This script requires that "some_module" be installed within the Python
-environment you are running this script in.
 """
 
 import sys
 import argparse
 import logging
 import pathlib
+#import re
 
-def find_files(search_dir: pathlib.Path, search_string: str) -> list[str]:
+def find_files(search_dir: pathlib.Path, search_string: str, newest_only: bool = False) -> list[str]:
     """
     Search for file containing search_string. Return a list of file names.
 
@@ -26,30 +26,31 @@ def find_files(search_dir: pathlib.Path, search_string: str) -> list[str]:
         The directory to start searching.
     search_string : str
         The string to match in the file name.
+    newest_only : bool
+        For each directory, only return the file with the latest timestamp.
 
     Returns
     -------
     list[str]
         A list of file name strings.
     """
-    file_match = re.compile(search_string)
+    # Find the files
+    #file_match = re.compile(re.escape(search_string), re.IGNORECASE)
     file_list = []
     for dir in pathlib.Path(search_dir).glob('**/'): # recursively get directories
         latest_file = None
-        for file in dir.glob('*'):
-            if file_match.search(file):
+        for file in dir.glob(search_string):
+            #if file_match.search(str(file)):
+            if newest_only:
                 if not latest_file:
                     latest_file = file
                 elif file.stat().st_mtime > latest_file.stat().st_mtime:
                     latest_file = file
-        if latest_file:
-            if '.xlsx' not in str(latest_file): # it is old '.xls' format
-                print('### File Error:', str(latest_file), '\n    This file has newest modified stamp but is ".xls" format')
-                err_count += 1
             else:
-                file_list.append(latest_file)
+                file_list.append(str(file))
+        if newest_only and latest_file:
+            file_list.append(str(latest_file))
 
-    print(len(file_list), 'issues list files found')
     return file_list
 
 def main() -> 1:
@@ -63,22 +64,31 @@ def main() -> 1:
     parser.add_argument('-d', '--search_dir', default=pathlib.Path().cwd(), type=pathlib.Path,
         help='The directory from where to start the search for matching files')
     parser.add_argument('-s', '--search_string', required=True, type=str,
-        help='The directory from where to start the search for matching files')
+        help='The string to search in the file names')
+    parser.add_argument('-n', '--newest_only', action='store_true',
+        help='For each directory, only return the file with the newest timestamp')
     args = parser.parse_args()
+    # Validate command line arguments (if necessary)
+    if not args.search_string.isascii() or '/' in args.search_string or '\\' in args.search_string:
+        logging.critical('--search_string is restricted to the ASCII character set, and cannot include "/" or "\\"')
+        return 1
     # Set up logging
-    if args.log_file is not None:
+    if args.log_file:
         # Note: filemode is set to clobber by default
         logging.basicConfig(format='%(asctime)s %(levelname)s:%(message)s', filename=args.log_file, filemode='w',
             level=args.log_level, datefmt='%Y/%m/%d %I:%M:%S')
     else:
         logging.basicConfig(format='%(levelname)s:%(message)s',
             level=args.log_level, stream=sys.stdout)
-    
 
-    # Functionality goes here
-    find_files(args.search_dir, args.search_string)
+    # Functionality starts here
+    file_list = find_files(args.search_dir, args.search_string, args.newest_only)
+    if not file_list:
+        print("No files found")
+    else:
+        for f in file_list:
+            print(f)
     return 0
 
-
 if __name__ == '__main__':
-    raise SystemExit(main())
+    raise sys.exit(main())
